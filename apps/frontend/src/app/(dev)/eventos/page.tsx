0274ce8e4d69377e8f2db92a89af28ed95c3f7ca
felipe mascena <felipe.mascena.dev@gmail.com>
2024-11-20 18:02:26 -0300

    atualização

--- apps/frontend/src/app/(dev)/eventos/page.tsx
+++ apps/frontend/src/app/(dev)/eventos/page.tsx
@@ -1,6 +1,7 @@
 import { eventos } from '@/core/constants';
 import Image from 'next/image';
 import Link from 'next/link';
+import QRCode from 'react-qr-code';
 
 export default function PaginaEventos() {
   return (
@@ -14,7 +15,7 @@ export default function PaginaEventos() {
         
         "
         >
-          <div className="relative w-full h-52">
+          <div className="relative w-full h-60">
             <Image
               src={evento.imagem}
               fill
@@ -25,6 +26,10 @@ export default function PaginaEventos() {
           <div className="flex-1 flex flex-col items-center p-7 gap-5 text-center">
             <span className="text-lg font-black">{evento.nome}</span>
             <p className="flex-1 text-sm text-zinc-400">{evento.descricao}</p>
+            <QRCode
+              value={JSON.stringify({ id: evento.id, senha: evento.senha })}
+              className="w-40 h-40"
+            />
             <div className="flex gap-5 ">
               <Link
                 href={`/evento/admin/${evento.id}/${evento.senha}`}
@@ -33,7 +38,7 @@ export default function PaginaEventos() {
                 Admin
               </Link>
               <Link
-                href={`/convite/admin/${evento.alias}`}
+                href={`/convite/${evento.alias}`}
                 className="flex-1 botao verde"
               >
                 Convite
