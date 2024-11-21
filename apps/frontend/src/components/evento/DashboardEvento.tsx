c8b30c6e9c745520ff51c83228cf99c80eebfa00
felipe mascena <felipe.mascena.dev@gmail.com>
2024-11-21 00:10:47 -0300

    Estruturando Dashboard de Eventos

--- apps/frontend/src/components/evento/DashboardEvento.tsx
+++ apps/frontend/src/components/evento/DashboardEvento.tsx
@@ -1,6 +1,7 @@
 import { Convidado, Evento } from '@/core';
 import InformacoesEvento from './InformacoesEvento';
 import AcessarViaQrCode from './AcessarViaQrCode';
+import Estatistica from '../shared/Estatistica';
 
 
 export interface DashboardEventoProps {
@@ -12,11 +13,28 @@ export interface DashboardEventoProps {
 
 export default function DashboardEvento(props: DashboardEventoProps) {
     return (
-        <div className='flex flex-col gap-2'>
-            <div className='flex gap-2'>
-                <InformacoesEvento evento={props.evento} className='flex-1' />
-                <AcessarViaQrCode evento={props.evento} className=''/>
-            </div>
+      <div className="flex flex-col gap-2">
+        <div className="flex gap-2">
+          <InformacoesEvento evento={props.evento} className="flex-1" />
+          <AcessarViaQrCode evento={props.evento} className="" />
         </div>
-    )
+        <div className="grid grid-cols-3 gap-6 mt-4">
+          <Estatistica
+            texto="Expectativa de Convidados:"
+            valor={props.evento.publicoEsperado}
+            imagem="/icones/convidados.svg"
+          />
+          <Estatistica
+            texto="Confirmações:"
+            valor={props.presentes.length}
+            imagem="/icones/confirmados.svg"
+          />
+          <Estatistica
+            texto="Total Confirmado:"
+            valor={props.totalGeral}
+            imagem="/icones/acompanhantes.svg"
+          />
+        </div>
+      </div>
+    );
 };
